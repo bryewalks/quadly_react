42fd8e32947ef286fb61c1e27c2902227618960f
brye <bryewalks@gmail.com>
2020-04-29 22:48:13 -0500

    added background image and none working buttons to home page

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
-
 import axios from 'axios'
 
 import { PageContainer } from 'Components/Globals'
 import { useLocation } from 'hooks'
+
+import { ButtonContainer, DroneBackground, WeatherButton } from './style'
 
 const Home = (props) => {
   const [weather, setWeather] = useState(null)
@@ -26,7 +27,7 @@ const Home = (props) => {
   }
   
   return (
-    <PageContainer>
+    <DroneBackground>
       { weather && <>
         <h2>Precipitation { weather.chance_of_precipitation }%</h2>
         <h2>Cloud Coverage { weather.cloud_cover }%</h2>
@@ -36,8 +37,12 @@ const Home = (props) => {
         <h2>wind speed { weather.wind_speed } MPH</h2>
         <h2>{ weather.good_to_fly ? "It's a good day to go flying!" : "Weather is not ideal for flying."}</h2>
       </>}
-      <button onClick={ handleCheckWeather }>Check Weather</button>
-    </PageContainer>
+      <ButtonContainer>
+        <WeatherButton onClick={ handleCheckWeather }>Check Weather</WeatherButton>
+        <WeatherButton onClick={ handleCheckWeather }>Check Weather</WeatherButton>
+        <WeatherButton onClick={ handleCheckWeather }>Check Weather</WeatherButton>
+      </ButtonContainer>
+    </DroneBackground>
   );
 }
 
